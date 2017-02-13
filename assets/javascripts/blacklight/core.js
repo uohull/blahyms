8c22819f205bbe593e2c3c72aa0d6cb944075394
John A Stephenson <john.stephenson@hull.ac.uk>
2017-02-13 13:55:13 +0000

    Add files via upload

---
+++ assets/javascripts/blacklight/core.js
@@ -0,0 +1,39 @@
+Blacklight = function() {
+  var buffer = new Array;
+  return {
+    onLoad: function(func) {
+      buffer.push(func);
+    },
+
+    activate: function() {
+      for(var i = 0; i < buffer.length; i++) {
+        buffer[i].call();
+      }
+    },
+
+    listeners: function () {
+      var listeners = [];
+      if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
+        // Turbolinks 5
+        if (Turbolinks.BrowserAdapter) {
+          listeners.push('turbolinks:load');
+        } else {
+          // Turbolinks < 5
+          listeners.push('page:load', 'ready');
+        }
+      } else {
+        listeners.push('ready');
+      }
+
+      return listeners.join(' ');
+    }
+  };
+}();
+
+// turbolinks triggers page:load events on page transition
+// If app isn't using turbolinks, this event will never be triggered, no prob. 
+$(document).on(Blacklight.listeners(), function() {
+  Blacklight.activate();  
+});
+
+
